a744c2b3ad3f5abd18e0cb5b33fe3111a0bc8afd
Greg1928 <gregoriosortino@protonmail.com>
2022-07-15 15:29:54 +0200

    add validation on update

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -90,6 +90,16 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
+        // validation
+        $request->validate([
+            'title' => 'required|max:100',
+            'description' => 'required',
+            'thumb' => 'nullable|max:500',
+            'price' => 'required|numeric|max:99.99',
+            'series' => 'required|max:100',
+            'sale_date' => 'required|after:01/01/1947|before:11/30/2023',
+            'type' => 'required|max:100',
+        ]);
         $data = $request->all();
         $comic->update($data);
 
